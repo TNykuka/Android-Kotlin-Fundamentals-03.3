2d923157e6e20cfb7d73adcf68fbea1333272307
TNy-Kk <kingkuka088@gmail.com>
2020-07-27 01:43:45 +0700

    Android Kotlin Fundamentals 03.3-4 Step2

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -101,7 +101,8 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         view.findNavController()
-                                .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment())
+                                .navigate(GameFragmentDirections
+                                        .actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
 
                     }
                 } else {
